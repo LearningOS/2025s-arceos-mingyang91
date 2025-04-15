9fab0abaf098c5fb0edaefa09d0a2196cd65e8b9
明扬 <my@famer.me>
2025-04-15 11:16:49 +0800

    rollback

--- scripts/total-test.sh
+++ scripts/total-test.sh
@@ -4,32 +4,26 @@ score=0
 
 if ./scripts/test-print.sh ; then
     ((score += 100))
-    printf "print pass\n" >&2
 fi
 
 if ./scripts/test-ramfs_rename.sh ; then
     ((score += 100))
-    printf "ramfs_rename pass\n" >&2
 fi
 
 if ./scripts/test-alt_alloc.sh ; then
     ((score += 100))
-    printf "alt_alloc pass\n" >&2
 fi
 
 if ./scripts/test-support_hashmap.sh ; then
     ((score += 100))
-    printf "support_hashmap pass\n" >&2
 fi
 
 if ./scripts/test-sys_map.sh ; then
     ((score += 100))
-    printf "sys_map pass\n" >&2
 fi
 
 if ./scripts/test-simple_hv.sh ; then
     ((score += 100))
-    printf "simple_hv pass\n" >&2
 fi
 
 echo $score
